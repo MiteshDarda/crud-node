52110731034028e267147273de3f158df7d6adf2
mitesh darda <mitesh.darda.0@gmail.com>
2023-03-21 00:22:24 +0530

    create users with blukCreate in userTable

--- dist/userTable.js
+++ dist/userTable.js
@@ -27,23 +27,24 @@ const userTable = async (values) => {
     let p = new Array;
     const t = await sequelize_1.default.transaction();
     await User.sync({ force: true });
-    for await (const value of values) {
-        try {
-            p.push(await User.create({ userName: value.name, age: value.age }));
-        }
-        catch {
-            p.push(Promise.reject());
-            break;
-        }
+    // for await(const value of values){
+    //     try{
+    //         p.push(await User.create({userName: value.name, age: value.age}));
+    //     }
+    //     catch{
+    //         break;
+    //     }
+    // }
+    // console.log([...values]);
+    try {
+        const users = await User.bulkCreate([...values]);
+        // console.log(users);
     }
-    Promise.all(p)
-        .then(res => {
-        console.log("INSIDE THEN", res, p);
-    })
-        .catch(err => {
-        console.log("INSIDE CATCH");
-        t.rollback();
-    });
+    catch (error) {
+        console.log("ERROR CANNOT CREATE NEW USERS, INCOMPLETE DATA");
+    }
+    // let data = await Promise.all(p)
+    // console.log("CONSOLE", data);
 };
 exports.userTable = userTable;
 //# sourceMappingURL=userTable.js.map
--- src/userTable.ts
+++ src/userTable.ts
@@ -23,24 +23,12 @@ const userTable = async (values) => {
     let p = new Array;
     const t = await sequelize.transaction()
     await User.sync({force:true})
-    for await(const value of values){
-        try{
-            p.push(await User.create({userName: value.name, age: value.age}));
-        }
-        catch{
-            p.push(Promise.reject());
-            break;
-        }
+    try{
+        const users = await User.bulkCreate([...values])
     }
-    
-    Promise.all(p)
-    .then( res => {
-        console.log("INSIDE THEN", res, p);
-    })
-    .catch (err => {
-        console.log("INSIDE CATCH");
-        t.rollback();
-    });
+    catch(error){
+        console.log("ERROR CANNOT CREATE NEW USERS, INCOMPLETE DATA");
+    }
 }
 
 export { User, userTable};
